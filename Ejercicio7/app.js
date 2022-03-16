8f34892fc9ba7053fc3243b5c7ffb475a5843887
Juan Carlos <93095797+JuanCarlosFR26@users.noreply.github.com>
2022-03-16 23:54:21 +0100

    Add files via upload

---
+++ Ejercicio7/app.js
@@ -0,0 +1,4 @@
+import { Coche, ford } from './coche.js';
+
+console.log(Coche);
+console.log(ford);
---
+++ Ejercicio7/coche.js
@@ -0,0 +1,11 @@
+
+export class Coche {
+    constructor(peso, potencia, marca) {
+        this.peso = peso;
+        this.potencia = potencia;
+        this.marca = marca;
+    }
+}
+export let ford = new Coche(1000, 600, "Ford");
+
+
